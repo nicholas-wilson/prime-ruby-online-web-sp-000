3cc8956520c66887a7ee8fa987f139ae0b41439c
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2019-11-20 00:59:52 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,9 @@
-# Add  code here!
+def prime?(number)
+  digits = [2, 3, 4, 5, 6, 7, 8, 9]
+  digits.each do |digit|
+    if (number % digit == 0)
+      return false
+    end
+  end
+  return true
+end
