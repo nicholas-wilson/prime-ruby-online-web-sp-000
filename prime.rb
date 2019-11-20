21d0848426ff703dd7b0cc856e088fe1662dd4c1
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2019-11-20 01:31:03 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime?(number)
+  if (number == 2 || number == 3 || number == 5 || number == 7)
+    return true
+  elsif (number <= 1)
+    return false
+  end
+  digits = (2..1000000).to_a
+  digits.each do |digit|
+    if (number != digit && number % digit == 0)
+      return false
+    end
+  end
+  return true
+end
